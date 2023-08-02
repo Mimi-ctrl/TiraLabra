8929a3dfa7acc8cd7f98b14b597ea1bd9a75f030
Laura Peltonen <laura.peltonen53@gmail.com>
2023-08-02 15:17:13 +0300

    win tests

--- src/tests/game_test.py
+++ src/tests/game_test.py
@@ -1,11 +1,47 @@
 import unittest
 from game import Game
 
-ROWS = 6
-COLUMN = 7
-
 
 class TestGame(unittest.TestCase):
     def setUp(self):
         self.game = Game()
-        self.board = Game.create_board()
+
+    def test_vertical_win(self):
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(0, 1)
+        self.assertEqual(self.game.check_win(), True)
+
+    def test_horizontal_win(self):
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(1, 1)
+        self.game.drop_piece(2, 1)
+        self.game.drop_piece(3, 1)
+        self.assertEqual(self.game.check_win(), True)
+
+    def test_positive_diagonal_win(self):
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(1, 1)
+        self.game.drop_piece(1, 1)
+        self.game.drop_piece(2, 1)
+        self.game.drop_piece(2, 1)
+        self.game.drop_piece(2, 1)
+        self.game.drop_piece(3, 1)
+        self.game.drop_piece(3, 1)
+        self.game.drop_piece(3, 1)
+        self.game.drop_piece(3, 1)
+        self.assertEqual(self.game.check_win(), True)
+
+    def test_negative_diagonal_win(self):
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(0, 1)
+        self.game.drop_piece(1, 1)
+        self.game.drop_piece(1, 1)
+        self.game.drop_piece(1, 1)
+        self.game.drop_piece(2, 1)
+        self.game.drop_piece(2, 1)
+        self.game.drop_piece(3, 1)
+        self.assertEqual(self.game.check_win(), True)
