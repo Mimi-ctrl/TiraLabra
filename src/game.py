c50e6e6c3f65f426593c48583ead4a24b5eae4b8
Laura Peltonen <laura.peltonen53@gmail.com>
2023-08-04 19:35:08 +0300

    fixing inputs

--- src/game.py
+++ src/game.py
@@ -10,15 +10,18 @@ class Game:
     def print_menu():
         """Print menu where user can choose new game or exit."""
         print("🄲 🄾 🄽 🄽 🄴 🄲 🅃  🄵 🄾 🅄 🅁")
-        print(
-            "\n--------------------------\n1: New Game\n2: Exit\n--------------------------")
+        print("\n--------------------------\n1: New Game\n2: Exit\n--------------------------")
         while True:
-            answer = int(input("\n--> "))
-            if answer == 1:
-                return True
-            if answer == 2:
-                print("\nBye!")
-                return False
+            answer = input("\n--> ")
+            if answer.isnumeric():
+                answer = int(answer)
+                if answer == 1:
+                    return True
+                if answer == 2:
+                    print("\nBye!")
+                    return False
+                print("\nInvalid input!")
+                continue
             print("\nInvalid input!")
             continue
 
@@ -27,11 +30,15 @@ class Game:
         """Print menu where user can choose which mode."""
         print("\n--------------------------\n1: Computer\n2: Player 2\n--------------------------")
         while True:
-            answer = int(input("\n--> "))
-            if answer == 1:
-                return 1
-            if answer == 2:
-                return 2
+            answer = input("\n--> ")
+            if answer.isnumeric():
+                answer = int(answer)
+                if answer == 1:
+                    return 1
+                if answer == 2:
+                    return 2
+                print("\nInvalid input!")
+                continue
             print("\nInvalid input!")
             continue
 
