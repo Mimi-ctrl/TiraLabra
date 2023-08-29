860893213f1d61775c3b1fc7ebe6ba6f2b2773e0
Laura Peltonen <laura.peltonen53@gmail.com>
2023-08-29 19:09:32 +0300

    More tests added

--- src/tests/ai_test.py
+++ src/tests/ai_test.py
@@ -1,5 +1,6 @@
+from io import StringIO
 import unittest
-from unittest.mock import patch, call
+from unittest.mock import patch
 import numpy as np
 from game import Game
 from ai import Ai
@@ -8,6 +9,7 @@ from ai import Ai
 class TestAi(unittest.TestCase):
     def setUp(self):
         self.ai = Ai()
+        self.game = Game()
 
     def test_initialization(self):
         self.assertTrue(np.array_equal(self.ai.board, np.zeros((6, 7))))
@@ -100,3 +102,79 @@ class TestAi(unittest.TestCase):
         ])
         score = self.ai.minimax(board, 4, -10**9, 10**9, True)
         self.assertIsInstance(score, int)
+
+    def test_game_loop(self):
+        with self.assertRaises(OSError):
+            self.ai.game_loop()
+
+    def test_drop_piece(self):
+        self.game.drop_piece(self.ai.board, 0, 1)
+        self.assertEqual(self.ai.board[5][0], 1)
+
+    def test_check_win(self):
+        self.ai.board[5][0] = 1
+        self.ai.board[5][1] = 1
+        self.ai.board[5][2] = 1
+        self.ai.board[5][3] = 1
+        self.assertTrue(self.game.check_win(self.ai.board))
+
+    def test_find_best_move_another_case(self):
+        board = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 2, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 2, 1, 2, 0],
+            [0, 0, 2, 1, 2, 1, 0]
+        ])
+        self.ai.board = board.copy()
+        best_move = self.ai.find_best_move(board)
+        self.assertEqual(best_move, 4)
+
+    def test_find_best_move_yet_another_case(self):
+        board = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 2, 0, 0, 0],
+            [0, 0, 2, 1, 0, 0, 0],
+            [0, 0, 1, 2, 0, 0, 0]
+        ])
+        self.ai.board = board.copy()
+        best_move = self.ai.find_best_move(board)
+        self.assertEqual(best_move, 2)
+
+    def test_find_best_move_different_configuration(self):
+        board = np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 2, 0, 0, 0],
+            [0, 0, 0, 1, 0, 0, 0]
+        ])
+        self.ai.board = board.copy()
+        best_move = self.ai.find_best_move(board)
+        self.assertEqual(best_move, 3)
+
+    def test_init(self):
+        self.assertTrue(np.array_equal(self.ai.board, np.zeros((6, 7))))
+        self.assertEqual(self.ai.player, 1)
+        self.assertIsNotNone(self.ai.game)
+
+    @patch('builtins.input', side_effect=['0', '1', '0', '1', '0', '1', '0', '1', '0', '1'])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_game_loop_valid_input(self, mock_stdout, mock_input):
+        self.ai.game_loop()
+        output = mock_stdout.getvalue().strip()
+        print("Final Output:\n", output)
+        self.assertNotIn("Invalid column.", output)
+        self.assertIn("Player  2 wins!", output)
+
+    @patch('builtins.input', side_effect=['0', '1', '2', '3', '4', '5', '6'])
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_game_loop_draw(self, mock_stdout, mock_input):
+        self.ai.board = np.ones((6, 7))
+        self.ai.game_loop()
+        output = mock_stdout.getvalue().strip()
+        self.assertIn("Draw match", output)
